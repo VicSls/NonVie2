4f743d223f2264fbdb0ba75844aa54d1a4a8ec96
VicSls <v.soulas@gmail.com>
2025-11-18 12:53:24 +0100

    yes

--- Nonvie.R
+++ Nonvie.R
@@ -13,180 +13,7 @@ library("plotrix")
 #Prepare the data
 data<-ulb_data
 data_clean <- data %>% filter(!is.na(claim_nb_tpl_md))
-#quest : este juste ? Estce qu'il faudrait pas aussi verif body injured ?
-
-
-#exposition au risque sup à 1
-Cdata <- data_clean %>%
-  mutate(admi_risk_exposure = pmin(admi_risk_exposure, 1))
-
-# Nous faisons cela pour conserver l'information (supposant que NA signifie 0 année sans sinistre)
-data_clean <- data_clean %>% 
-  mutate(veh_years_claim_free = ifelse(is.na(veh_years_claim_free), 0, veh_years_claim_free))
-
-#categoriel to numeric
-data_clean <- data_clean %>% 
-  mutate(
-    claim_nb_tpl_md = as.numeric(claim_nb_tpl_md), 
-    claim_nb_tpl_bi = as.numeric(claim_nb_tpl_bi)  
-  )
-#Ajouter tot_claim
-data_clean <- data_clean %>%
-  mutate(Tot_claim = if_else(
-    claim_nb_tpl_md == 1 | claim_nb_tpl_bi == 1, 
-    1, 
-    0
-  ))
-
-###############################################################################
-summary(data_clean)
-#Distributions of the data
-data_clean %>%
-  dplyr::select(where(is.numeric),-id,-geo_postcode_lng,-geo_postcode_lat,-driv_y_add_flg) %>%
-  pivot_longer(cols = everything()) %>%
-  ggplot(aes(x = value)) +
-  geom_histogram(bins = 30, fill = "steelblue", alpha = 0.7) +
-  facet_wrap(~ name, scales = "free") +
-  labs(title = "Histograms of Numerical Variables") +
-  theme_minimal()
-
-#Intro: tests sur les v.a.
-
-#Nbr total d'exposition aux risques en année
-format(sum(data_clean$admi_risk_exposure))
-
-#Nbr de police par mois d'exposition
-table(cut(data_clean$admi_risk_exposure, breaks = seq(from = 0, to = 1,by = 1/12), labels = 1:12))
-
-#% du portefeuille par mois d'exposition
-round(prop.table(table(cut(data_clean$admi_risk_exposure, breaks = seq(from = 0, to = 1,by = 1/12), labels = 1:12))), 4)
-
-#graph du nbr d'exposure 
-Exposure.summary = cut(data_clean$admi_risk_exposure, breaks = seq(from = 0, to = 1,by = 1/12))
-levels(Exposure.summary) = 1:12
-ggplot()+geom_bar(aes(x=Exposure.summary)) + xlab("Number of months") + ggtitle("Exposure in months")
-
-#Nbr claim
-ggplot(data_clean, aes(x=Tot_claim))+geom_bar()+
-  geom_text(stat='count', aes(label=..count..), vjust=-1)+ylim(c(0,210000))+
-  ylab("")+ xlab("Number of Claims")+  ggtitle("Proportion of policies by number of claims")
-
-#We can compute the average claim frequency in this portfolio, taking into account the different exposures.
-#resultat rejoint les 5% du prof
-
-#Observed claim frequencysum(data_clean$Tot_claim) / sum(data_clean$admi_risk_exposure)
-
-#veh_power
-#nbr total de véhicule par puissance
-table(data_clean$veh_power)
-
-#Exposition des véhicules par puissance 
-
-Power.summary = ddply(data_clean, .(veh_power), summarize, totalExposure = sum(admi_risk_exposure), Number.Observations= length(admi_risk_exposure))
-
-ggplot(Power.summary, aes(x=veh_power, y=totalExposure, fill=veh_power)) + 
-  geom_bar(stat="identity")+
-  ylab("Exposure in years")+
-  geom_text(stat='identity', aes(label=round(totalExp, 0), color=veh_power), vjust=-0.5)+
-  guides(fill=FALSE, color=FALSE)
-
-#Nbr de claim en proportion de l'exposition au risque
-Power.summary = ddply(data_clean, .(veh_power), summarize, totalExposure = sum(admi_risk_exposure), Number.Observations = length(admi_risk_exposure), Number.Claims = sum(Tot_claim), Obs.Claim.Frequency = sum(Tot_claim)/sum(admi_risk_exposure))
-ggplot(Power.summary, aes(x=veh_power, y=Obs.Claim.Frequency, fill=veh_power)) + 
-  geom_bar(stat="identity")+
-  ylab("Nbr de claim en proportion de l'exposition au risque")+
-  geom_text(stat='identity', aes(label=round(totalExposure, 0), color=veh_power), vjust=-0.5)+
-  guides(fill=FALSE, color=FALSE)
-
-#Same+ ligne rouge qui correspond à la moyenne du portefeuille
-portfolio.cf = sum(data_clean$Tot_claim)/ sum(data_clean$admi_risk_exposure)
-ggplot(Power.summary) + geom_bar(stat="identity", aes(x=veh_power, y=Obs.Claim.Frequency, fill=veh_power)) + 
-  geom_line(aes(x = as.numeric(veh_power),y=portfolio.cf), color="red") + guides(fill=FALSE)
-
-#veh_age
-#Nbr tot, mais pas relater à l'exposion
-ggplot(data_clean, aes(x=veh_age)) + geom_bar()  + xlab("Age of the Car")
-#Par rapport à l'exposition, on voit que les plots se ressemblent
-CarAge.summary = ddply(data_clean, .(veh_age), summarize, totalExposure = sum(admi_risk_exposure), Number.Observations = length(admi_risk_exposure))
-ggplot(CarAge.summary, aes(x=veh_age, y=totalExposure)) + geom_bar(stat='identity') + ylab("Exposure in years")
-
-ggplot(data_clean[data_clean$veh_age==0,], aes(x="Exposure", y=admi_risk_exposure)) + geom_boxplot() +ggtitle("Exposure of new cars")
-
-CarAge.summary = ddply(data_clean, .(veh_age), summarize, totalExposure = sum(admi_risk_exposure), Number.Observations = length(admi_risk_exposure), Number.Claims = sum(Tot_claim), Obs.Claim.Freq = sum(Tot_claim)/sum(admi_risk_exposure))
-ggplot(CarAge.summary) + geom_bar(stat="identity", aes(x=veh_age, y=Obs.Claim.Freq, fill=veh_age)) + 
-  geom_line(aes(x = as.numeric(veh_age),y=portfolio.cf), color="red") + guides(fill=FALSE)
-
-#driv age
-DriverAge.summary = ddply(data_clean, .(driv_m_age), summarize, totalExposure = sum(admi_risk_exposure), Number.Observations = length(admi_risk_exposure), Number.Claims = sum(Tot_claim), Obs.Claim.Freq = sum(Tot_claim)/sum(admi_risk_exposure))
-
-#We can show the Exposures by Age of the Driver
-ggplot(DriverAge.summary, aes(x=driv_m_age, y=totalExposure)) + geom_bar(stat='identity', width=0.8) + ylab("Exposure in years")+xlab("Age of the Driver")
-
-#Observed claim frequency
-ggplot(DriverAge.summary) + geom_bar(stat="identity", aes(x=driv_m_age, y=Obs.Claim.Freq, fill=driv_m_age)) + 
-  geom_line(aes(x = as.numeric(driv_m_age),y=portfolio.cf), color="red") + guides(fill=FALSE)
-
-
-#Brand
-Brand.summary = ddply(data_clean, .(veh_make), summarize, totalExposure = sum(admi_risk_exposure), Number.Observations = length(admi_risk_exposure), Number.Claims = sum(Tot_claim), Obs.Claim.Freq = sum(Tot_claim)/sum(admi_risk_exposure))
-ggplot(Brand.summary, aes(x=reorder(veh_make,totalExposure), y=totalExposure, fill=veh_make)) +
-  geom_bar(stat='identity') +
-  coord_flip()+guides(fill=FALSE)+xlab("")+ylab("Exposure in years")
-
-ggplot(Brand.summary, aes(x=reorder(veh_make,Obs.Claim.Freq), y=Obs.Claim.Freq, fill=veh_make)) +
-  geom_bar(stat='identity') +
-  coord_flip()+guides(fill=FALSE)+ ggtitle("Observed Claim Frequencies by Brand of the car")+xlab("")+ylab("Observed Claim Frequency")
-#ressemble à une approximation de la puissance véhicule ???
-
-
-#Gas
-Gas.summary = ddply(data_clean, .(veh_fuel), summarize, totalExposure = sum(admi_risk_exposure), Number.Observations = length(admi_risk_exposure), Number.Claims = sum(Tot_claim), Obs.Claim.Freq = sum(Tot_claim)/sum(admi_risk_exposure))
-ggplot(Gas.summary, aes(x=veh_fuel, y=totalExposure, fill=veh_fuel)) + geom_bar(stat="identity") + guides(fill=FALSE)
-#Usually diesel has a bigger impact portfolio, here hybrid and electricity are still big
-ggplot(Gas.summary, aes(x=veh_fuel, y=Obs.Claim.Freq, fill=veh_fuel)) + geom_bar(stat="identity") + guides(fill=FALSE)
-
-#Postcode or geo_munty_fr or coordonnée ?
-sapply(
-  data_clean[, c("geo_postcode_2digits", "geo_munty_fr")],
-  function(x) length(unique(x))
-)
-n_unique_coords <- nrow(
-  distinct(data_clean, geo_postcode_lat, geo_postcode_lng)
-)
-n_unique_coords
-#Donc geo_postcode_2digits est enough
-Postcode.summary = ddply(data_clean, .(geo_postcode_2digits), summarize, totalExposure = sum(admi_risk_exposure), Number.Observations = length(admi_risk_exposure), Number.Claims = sum(Tot_claim), Obs.Claim.Freq = sum(Tot_claim)/sum(admi_risk_exposure))
-twoord.plot(1:40,Postcode.summary$totalExposure,1:40,Postcode.summary$Obs.Claim.Freq,xlab="Region",
-            rylim=c(0,0.1),type=c("bar","p"), xticklab = Postcode.summary$geo_postcode_2digits, ylab = "Exposure", rylab = "Observed Claim Frequency")
-
-#2nd selection dataset de ce qu'on a besoin et rename car nom horrible
-Cdata <- data_clean %>% 
-  dplyr::select(
-    -geo_munty_fr, 
-    -geo_province_fr, 
-    -geo_region_fr, 
-    -geo_postcode_lat,
-    -geo_postcode_lng, 
-    -id
-  ) 
-##########################################################################
-library("readxl")
-library("dplyr")
-library("ggplot2")
-library("MASS")
-library("mgcv")
-library("caret")
-library("gridExtra")
-library("parallel")
-library("tidyr")
-library("plyr")
-library("plotrix")
-
-#Prepare the data
-data<-ulb_data
-data_clean <- data %>% filter(!is.na(claim_nb_tpl_md))
 data_clean <- data_clean %>% filter(!is.na(claim_nb_tpl_bi))
-
 
 #exposition au risque sup à 1
 data_clean <- data_clean %>%
@@ -205,6 +32,7 @@ data_clean <- data_clean %>%
   )
 
 ###############################################################################
+
 summary(data_clean)
 #Distributions of the data
 data_clean %>%
@@ -232,10 +60,12 @@ Exposure.summary = cut(data_clean$admi_risk_exposure, breaks = seq(from = 0, to 
 levels(Exposure.summary) = 1:12
 ggplot()+geom_bar(aes(x=Exposure.summary)) + xlab("Number of months") + ggtitle("Exposure in months")
 
+
 #Nbr claim
 ggplot(data_clean, aes(x=Tot_claim))+geom_bar()+
   geom_text(stat='count', aes(label=..count..), vjust=-1)+ylim(c(0,210000))+
   ylab("")+ xlab("Number of Claims")+  ggtitle("Proportion of policies by number of claims")
+
 
 #We can compute the average claim frequency in this portfolio, taking into account the different exposures.
 #resultat rejoint les 5% du prof
@@ -339,6 +169,7 @@ Cdata <- data_clean %>%
   ) 
 #on enlève purpose car 1 seul classe, on a perdu les autres classes 
 #car ils étaient NA
+
 ##########################################################################
 #GAM claim_nb_tpl_md
 
